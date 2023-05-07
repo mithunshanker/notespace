e54f82d9c220c6a3207d9c5b7f212d7c1e4be220
mithunshanker <87591451+mithunshanker@users.noreply.github.com>
2023-05-07 21:07:38 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -308,27 +308,27 @@ Overall, our website is a valuable resource for JEE & NEET aspirants who want to
     <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
         <nav className="flex flex-wrap justify-center -mx-5 -my-2">
             <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                <a href="/" className="text-base leading-6 text-gray-500 hover:text-gray-900">
                     About
                 </a>
             </div>
             <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                <a href="/blog" className="text-base leading-6 text-gray-500 hover:text-gray-900">
                     Blog
                 </a>
             </div>
             <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                <a href="/" className="text-base leading-6 text-gray-500 hover:text-gray-900">
                     Team
                 </a>
             </div>
             {/* <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                <a href="/" className="text-base leading-6 text-gray-500 hover:text-gray-900">
                     Pricing
                 </a>
             </div> */}
             <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                <a href="/" className="text-base leading-6 text-gray-500 hover:text-gray-900">
                     Contact
                 </a>
             </div>
