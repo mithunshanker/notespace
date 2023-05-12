7630d12e30b48069f0448cbac756c395d8a05551
mithunshanker <87591451+mithunshanker@users.noreply.github.com>
2023-05-12 11:52:56 +0530

    Update post.js

--- pages/api/post.js
+++ pages/api/post.js
@@ -3,7 +3,13 @@ import cors from 'cors'
 
 const corsMiddleware = cors({
   // Set the origin to allow requests from the specified domain
-  origin: 'https://notesspace.netlify.app/'
+  origin: 'https://notesspace.netlify.app',
+  // Set the allowed methods to include POST and GET
+  methods: ['POST', 'GET'],
+  // Set the allowed headers to include the Content-Type header
+  allowedHeaders: ['Content-Type'],
+  // Set the credentials option to true if you need to allow cookies or authorization headers
+  credentials: true
 });
 
 export default async function handler(req, res) {
