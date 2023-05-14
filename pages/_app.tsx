2f0237e61d2f047c1602565a2755978126050929
mithunshanker <87591451+mithunshanker@users.noreply.github.com>
2023-05-14 10:23:06 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,12 @@
 import { AppProps } from 'next/app'
-import Script from 'next/script'
+
 import '../styles/globals.css'
-import Head from 'next/head'
+
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
-<Head>
-   <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
-<Script strategy="lazyOnload">
-    {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-        });
-    `}
-</Script>
-  </Head>
   <Component {...pageProps} /></>
 }
 
